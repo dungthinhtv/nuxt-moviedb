9f4c09547c2384e47086d71d319f7213c8942af7
dungthinhtv <102164139+dungthinhtv@users.noreply.github.com>
2022-09-20 20:59:00 +0700

    2 files modified

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,43 @@
 <template>
-  <div>Footer</div>
+  <v-footer dark padless>
+    <v-card class="flex text-center" flat tile dense>
+      <v-card-title class="teal">
+        <strong class="subheading"
+          >Get connected with us on social networks!</strong
+        >
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          v-for="(icon, index) in icons"
+          :key="index"
+          class="mx-4"
+          dark
+          icon
+          :to="icon.link"
+        >
+          <v-icon size="24px">
+            {{ icon.ic }}
+          </v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-card-text class="py-2 white--text text-center">
+        {{ new Date().getFullYear() }} — <strong>MovieDB</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    icons: [
+      { ic: 'mdi-facebook', link: 'https://facebook.com' },
+      { ic: 'mdi-twitter', link: 'https://twitter.com' },
+      { ic: 'mdi-linkedin', link: 'https://linkedin.com' },
+      { ic: 'mdi-instagram', link: 'https://instagram.com' },
+    ],
+  }),
+};
 </script>
-
-<style></style>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,17 +3,36 @@
     <v-toolbar>
       <v-toolbar-title>
         <v-icon color="pink" size="64px">mdi-video</v-icon>
+        MovieDB
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items>
-        <v-btn text link>Movies</v-btn>
+        <v-btn
+          text
+          link
+          v-for="(item, index) in items"
+          :key="index"
+          :to="item.link"
+        >
+          {{ item.title }}</v-btn
+        >
+        <!-- <v-btn text link>TV Series</v-btn>
+        <v-btn text link>Actor</v-btn> -->
       </v-toolbar-items>
     </v-toolbar>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    items: [
+      { title: "Movies", link: "/movies" },
+      { title: "TV Series", link: "/tv-series" },
+      { title: "Actors", link: "/actors" },
+    ],
+  }),
+};
 </script>
 
 <style></style>
