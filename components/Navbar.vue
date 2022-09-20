aecca38f1618425bed7ef476480e112718eeed10
dungthinhtv <102164139+dungthinhtv@users.noreply.github.com>
2022-09-20 20:29:14 +0700

    4 files modified, 2 added, 3 deleted

---
+++ components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-toolbar>
+      <v-toolbar-title>
+        <v-icon color="pink" size="64px">mdi-video</v-icon>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn text link>Movies</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
