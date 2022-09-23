0aca27d189d4256d9e0f8b7a53faa9cbcf3c3ed6
dungthinhtv <102164139+dungthinhtv@users.noreply.github.com>
2022-09-23 15:26:50 +0700

    1 file modified

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['/plugins/loadFirstData.js'],
+  plugins: ['~/plugins/loadFirstData.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
