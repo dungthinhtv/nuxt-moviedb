1bc66987468613c4fedc198f715093584518354e
dungthinhtv <dungthinhtv@gmail.com>
2023-03-30 14:17:56 +0700

    asdsad

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,9 +3,9 @@ require('dotenv').config()
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  // ssr: false,
 
-  // mode: 'universal',
+  mode: 'universal',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
