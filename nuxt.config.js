608764b61d4753b57229f02c27c6d22c500ec92b
dungthinhtv <dungthinhtv@gmail.com>
2023-03-30 13:56:41 +0700

    ssacda

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/loadFirstData.js','~plugins/vuetify.js'],
+  plugins: ['~/plugins/loadFirstData.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -94,6 +94,5 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    vendor: ['vuetify']
   },
 };
